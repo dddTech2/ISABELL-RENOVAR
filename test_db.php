136c47fbf6aded6b5f10199afc57884483e64a2c
Your Name <you@example.com>
2026-05-12 15:38:35 -0500

    chore: remove temp test files

--- test_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once "libs/paloSantoDB.class.php";
-$dsn = "mysql://asterisk:asterisk@localhost/asterisk";
-$db = new paloDB($dsn);
-print_r($db->fetchTable("SHOW TABLES LIKE 'ps_%'"));
-print_r($db->fetchTable("SELECT * FROM ps_auths LIMIT 1"));
-print_r($db->fetchTable("SELECT data FROM sip WHERE keyword='secret' LIMIT 1"));
--- test_pjsip.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "libs/paloSantoDB.class.php";
-require_once "libs/paloSantoConfig.class.php";
-$dsnAsterisk = "mysql://asterisk:asterisk@localhost/asterisk";
-$dbAsterisk = new paloDB($dsnAsterisk);
-print_r($dbAsterisk->fetchTable("SELECT id, password FROM ps_auths WHERE id LIKE '%1005%'", TRUE));
